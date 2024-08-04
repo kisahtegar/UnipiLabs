87131cdf76d77cff59d2699f054f67733954b7c6
Kisah Tegar Putra Abdi <code.kisahtegar@gmail.com>
2024-08-04 19:27:45 +0700

    adding GudangKisah.

--- warung/gen/com/example/warung/R.java
+++ warung/gen/com/example/warung/R.java
@@ -24,15 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int TambahData=0x7f080006;
         public static final int action_settings=0x7f080008;
         public static final int harga=0x7f080003;
         public static final int id_produk=0x7f080000;
         public static final int jenis=0x7f080002;
-        public static final int listView=0x7f080006;
-        public static final int name=0x7f080001;
+        public static final int listView=0x7f080007;
+        public static final int nama=0x7f080001;
         public static final int persediaan=0x7f080004;
         public static final int submit_button=0x7f080005;
-        public static final int tambah_button=0x7f080007;
     }
     public static final class layout {
         public static final int activity_add_data=0x7f030000;
@@ -44,14 +44,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int harga=0x7f050006;
         public static final int hello_world=0x7f050002;
-        public static final int id_produk=0x7f050003;
-        public static final int jenis=0x7f050005;
-        public static final int nama=0x7f050004;
-        public static final int persediaan=0x7f050007;
-        public static final int submit=0x7f050008;
-        public static final int tambah=0x7f050009;
     }
     public static final class style {
         /** 
--- warung/src/com/example/warung/MainActivity.java
+++ warung/src/com/example/warung/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.warung;
 
-import java.util.ArrayList;
-
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
 import android.os.Bundle;
 import android.os.Handler;
-import android.app.Activity;
-import android.util.Log;
-import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Button;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,6 +20,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
@@ -33,40 +36,41 @@ import org.apache.http.params.HttpParams;
 
 public class MainActivity extends Activity {
 	
+	private Button TambahData;
 	private ListView listView;
 	private ArrayAdapter<String> adapter;
 	private ArrayList<String> barangList;
 	private Handler handler = new Handler();
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		// Initialize
+		TambahData = (Button)findViewById(R.id.TambahData);
 		listView = (ListView) findViewById(R.id.listView);
 		barangList = new ArrayList<String>();
-		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, barangList);
+		adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,barangList);
 		listView.setAdapter(adapter);
 		
 		getData();
+		
+		TambahData.setOnClickListener(new View.OnClickListener() {
+			
+			
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, AddDataActivity.class);
+				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(intent);
+			}
+		});
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
+	
 	private void getData() {
-		Thread thread = new Thread(new Runnable() {
+		Thread thread = new Thread(new Runnable(){
 			@Override
-			public void run() {
-				String urlString = "https://192.168.56.5/warung/getData.php";
-				
-//				String urlString = "https://bad5-182-3-50-189.ngrok-free.app/warung/getData.php";
-				
+			public void run(){
+				String urlString = "http://192.168.193.5:8081/warung/getData.php";
 				try {
 					URL url = new URL(urlString);
 					HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
@@ -75,50 +79,46 @@ public class MainActivity extends Activity {
 					BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 					StringBuilder stringBuilder = new StringBuilder();
 					String line;
-					
-					
-					while ((line = reader.readLine()) != null) {
+					while ((line = reader.readLine()) !=null){
 						stringBuilder.append(line);
-						Log.d("MainActivity", line);
 					}
 					
 					reader.close();
 					urlConnection.disconnect();
+					
 					parseJSON(stringBuilder.toString());
 					
-				} catch (Exception e) {
+				} catch (Exception e){
 					e.printStackTrace();
 				}
 			}
 		});
-		
 		thread.start();
 	}
 	
-	private void parseJSON(final String json) {
-		handler.post(new Runnable() {
+	private void parseJSON (final String json){
+		handler.post(new Runnable(){
 			@Override
-			public void run() {
-				try {
+			public void run(){
+				try{
 					JSONArray jsonArray = new JSONArray(json);
-					for (int i = 0; i < jsonArray.length(); i++) {
+					for (int i = 0; i < jsonArray.length(); i++){
 						JSONObject jsonObject = jsonArray.getJSONObject(i);
-						String kode = jsonObject.getString("kode");
-						String nama = jsonObject.getString("nama");
-						String jenis = jsonObject.getString("jenis");
+						String kode = jsonObject.getString("id_produk");
+						String nama = jsonObject.getString("nama_produk");
 						String harga = jsonObject.getString("harga");
 						String stok = jsonObject.getString("stok");
+						String jenis = jsonObject.getString("jenis");
 						
-						String item = "Kode: " + kode + "\nNama: " + nama + "\nJenis" + jenis + "\nHarga" + harga + "\nStoks" + stok;
-						
+						String item ="Kode : " + kode + "\nNama : " + nama + "\nHarga : " + harga + "\nStok : " + stok + "\nJenis : " + jenis;
 						barangList.add(item);
 					}
-					
 					adapter.notifyDataSetChanged();
-				} catch (JSONException e) {
+				}catch (JSONException e){
 					e.printStackTrace();
 				}
 			}
 		});
 	}
+	
 }
